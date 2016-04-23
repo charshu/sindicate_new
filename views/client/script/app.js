04cde00b74bd296623818d46fc2d668fc497a5d1
Vachirakorn Rungsikawanich <vachirakorn@hotmail.com>
2016-04-23 14:34:07 +0700

    เพิ่มเมนู student track

--- views/client/script/app.js
+++ views/client/script/app.js
@@ -9,3 +9,74 @@ app.controller("MainController", ["$scope", function($scope){
 }
 
 ]);
+
+app.controller("modalController", ["$scope", function($scope){
+  $scope.modal_btn_text = "เพิ่มเติม";
+  $scope.prefixtitle = "แฟ้มข้อมูลของ";
+    $scope.students = [{
+      // left
+      id:'5630520921',
+      name: "วชิรกรณ์ รังสิกวานิช",
+      gpax: "3.27",
+      gpa:"3.47",
+      attendScore:"88",
+      isflagged:true,
+      note:"มาเรียนตรงเวลา ตั้งใจเรียนทุกวัน",
+      imgUrl:'/images/mypic.jpg',
+      fbUrl:'https://www.facebook.com/chi.zzwzz',
+
+      //-- right
+      tel:'0882391875',
+      parentTel:"0814295635",
+      gender:'ชาย',
+      faculty:'วิศวกรรมศาสตร์',
+      department:'วิศวกรรมคอมพิวเตอร์',
+      degree:"ปริญญาตรี",
+      year:"3",
+      birthdate:'26-10-2536'
+    },
+    {
+      id:'5630513521',
+      name: "รุจิรา รุจิสมบัติ",
+      gpax: "3.46",
+      attendScore:"99",
+      isflagged:false,
+      note:"มาเรียนตรงเวลา ตั้งใจเรียนทุกวัน",
+      imgUrl:'/images/mypic.jpg',
+      fbUrl:'https://www.facebook.com/chi.zzwzz',
+
+      // profilestuid:'5630520921',
+      tel:'0882391875',
+      parentTel:"0814295635",
+      gender:'ชาย',
+      faculty:'วิศวกรรมศาสตร์',
+      department:'วิศวกรรมคอมพิวเตอร์',
+      degree:"ปริญญาตรี",
+      year:"3",
+      birthdate:'26-10-2536'
+    },
+    {
+      id:'5630722121',
+      name: "วชิรกรณ์ รังสิกวานิช",
+      gpax: "3.27",
+      attendScore:"88",
+      isflagged:false,
+      note:"มาเรียนตรงเวลา ตั้งใจเรียนทุกวัน",
+      imgUrl:'/images/mypic.jpg',
+      fbUrl:'https://www.facebook.com/chi.zzwzz',
+
+      // profilestuid:'5630520921',
+      tel:'0882391875',
+      parentTel:"0814295635",
+      gender:'ชาย',
+      faculty:'วิศวกรรมศาสตร์',
+      department:'วิศวกรรมคอมพิวเตอร์',
+      degree:"ปริญญาตรี",
+      year:"3",
+      birthdate:'26-10-2536'
+    }];
+
+
+}
+
+]);
