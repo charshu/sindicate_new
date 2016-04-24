7129a6568e9600d6461cfda842ed8b8ad75b1133
Vachirakorn Rungsikawanich <vachirakorn@hotmail.com>
2016-04-24 18:55:33 +0700

    server version

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+         'password': 'rghpj8wb'
+    },
+	'database': 'sindicate2',
+    'users_table': 'users'
+};
